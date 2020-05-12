4eede36ee367aea2cc4978e0f4cac8f3a113cd62
Graeme Gorman <ggorman@3storysoftware.com>
2020-05-12 15:21:22 -0400

    add missing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {TableModule} from 'primeng/table';
+import { Test1Component } from './test1/test1.component';
+import { CarService } from './car.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, SimpleChanges, OnChanges, ViewChild } from '@angular/core';
+import { CarService, Car } from '../car.service';
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.less']
+})
+export class Test1Component implements OnInit {
+  @ViewChild('turboTable', { static: false }) turboTable: any;
+  cars: Car[];
+  paging: number;
+  constructor(private carService: CarService) { }
+
+  ngOnInit(): void {
+
+    const c = this.carService.getCars();
+    this.paging = 50;
+    const c2: Car[] = c;
+    this.cars = c2.slice(0, 5);
+
+    window.setTimeout(() => {
+      for (let x = 0; x < 100; x++){
+        this.cars.push(this.carService.getCars()[x]);
+      }
+      //this.turboTable._totalRecords = 105;
+    }, 5000);
+  }
+}
